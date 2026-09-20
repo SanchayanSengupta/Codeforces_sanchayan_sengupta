76376c5ac7781c1cb398158acd721b1c250f663b
Sanchayan Sengupta <sanchayan890@gmail.com>
2026-09-20 23:55:30 +0600

    Update B_All_Distinct.cpp

--- Div 4/Codeforces Round 799 (Div. 4)/B_All_Distinct.cpp
+++ Div 4/Codeforces Round 799 (Div. 4)/B_All_Distinct.cpp
@@ -1,57 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <set>
 using namespace std;
 
-int main(){
+int main() {
     int t;
-     cin >> t;
+    cin >> t;
 
-     while(t--){
+    while (t--) {
         int n;
         cin >> n;
 
-        int arr[n];
-        for(int i=0; i<n;i++){
-            cin >> arr[i];
-        }
-
-        int temp[n];
-        for(int i=0; i<n;i++){
-            temp[i] = arr[i];
-        }
-
-        int count = 0;
+        set<int> dis;
 
         for(int i=0; i<n;i++){
-            for(int j=i+1; j<n;j++){
-                    if(arr[i] == arr[j]){
-                    //count++;
-                    arr[i] = 0;
-                    arr[j] = 0;
-                }
-            }
+            int x;
+            cin >> x;
+            dis.insert(x);
         }
 
-        for(int i=0; i<n; i++){
-            if(arr[i] != 0){
-                count++;
-            }
-        }
+        int dist = dis.size();
+        int elem = n - dist;
 
-        if(count ==  0 && n%2 == 0 && n>2){
-            cout << n/2 << endl;
-        }
-        else if(count ==  0 && n%2 == 0 && n<=2){
-            cout << n-n << endl;
+        if(elem % 2 ==0){
+            cout << dist << endl;
         }
         else{
-            cout << count << endl;
+            cout << dist - 1 << endl;
         }
 
-        // for(int val:arr){
-        //     cout << val << " ";
-        // }
-        // cout << endl;
+    }
 
-        //cout << count << endl;
-     }
+    return 0;
 }
